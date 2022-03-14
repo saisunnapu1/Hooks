5788c9bc4dbe419715739ee24e0148f0ee658935
saisunnapu1 <88183158+saisunnapu1@users.noreply.github.com>
2022-03-14 14:06:12 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Hello</h1>
       <h2>I am developer</h2>
       <h1>i am from india</h1>
-      <h3>Australia</h3>
+      <h3>Australia-1122</h3>
       <Counter />
     </div>
   );

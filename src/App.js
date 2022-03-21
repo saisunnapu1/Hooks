73ec0dc2f8b84c7610fc1844f56e1d27aad258b0
saisunnapu1 <88183158+saisunnapu1@users.noreply.github.com>
2022-03-21 13:33:37 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import { Provider } from "react-redux";
 import "./styles.css";
 import Weather from "./Weather";
 import store from "../redux/fakeApi/store";
+import store1 from "../redux/weather/store";
 import UserContainer from "../redux/fakeApi/UserContainer";
 import UserContainer1 from "../redux/fakeApi/UserContainer1";
+import ReduxWeather from "../redux/weather/ReduxWeather";
 
 export default function App() {
   return (
@@ -12,9 +14,12 @@ export default function App() {
 
     //   <Weather />
     // </div>
-    <Provider store={store}>
-      {/* <UserContainer /> */}
-      <UserContainer1 />
+    // <Provider store={store}>
+    //   {/* <UserContainer /> */}
+    //   <UserContainer1 />
+    // </Provider>
+    <Provider store={store1}>
+      <ReduxWeather />
     </Provider>
   );
 }
